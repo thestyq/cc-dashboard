db87e3c73db015dfeb147981b8dbdf72b34d79ac
Tomasz Styczen <styczen.tom@gmail.com>
2017-07-23 11:55:23 +0200

    initial commit

---
+++ src/main/java/com/ts/ccdashboard/CcDashboardApplication.java
@@ -0,0 +1,12 @@
+package com.ts.ccdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcDashboardApplication.class, args);
+	}
+}
